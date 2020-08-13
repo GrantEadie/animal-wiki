5b8e829f6a120b56d694cd8afadb63a1f38690e2
Grant Eadie <grantleadie@gmail.com>
2020-08-13 10:54:43 -0700

    prompts user to pick an animal and drops down photo and image of selected animal.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("li#platypus").click(function() {    
+    $("li#platypus p").toggle("hidden");
+  });
+  $("li#cheetah").click(function() {    
+    $("li#cheetah p").toggle("hidden");
+  });
+
+  $("li#pickle").click(function() {    
+    $("li#pickle p").toggle("hidden");
+  });
+
+});
